571ad6a4eece16a367031ae37dfda3fa7b83f5f0
Daniel Mason <daniel@danielmason.com>
2024-12-04 14:17:27 +0000

    cant work out why d3 p2 is not working, will come back

--- src/day03.rs
+++ src/day03.rs
@@ -1,32 +1,175 @@
 use crate::solution::Solution;
+use nom::character::complete::{anychar, digit1, not_line_ending};
+use nom::combinator::map_res;
+use nom::multi::{many1, many_till};
+use nom::sequence::{delimited, separated_pair};
+use nom::{IResult, Parser};
+use nom_supreme::error::ErrorTree;
+use nom_supreme::tag::complete::tag;
+use nom_supreme::ParserExt;
 
 pub struct AdventPuzzle;
 
 impl Solution for AdventPuzzle {
-    fn part1(_input: &str) -> String {
-        todo!()
+    fn part1(input: &str) -> String {
+        input
+            .lines()
+            .map(|line| run_line_basic(line))
+            .sum::<usize>()
+            .to_string()
     }
 
-    fn part2(_input: &str) -> String {
-        todo!()
+    fn part2(input: &str) -> String {
+        input
+            .lines()
+            .map(|line| run_line_do_dont(line))
+            .sum::<usize>()
+            .to_string()
     }
+}
+
+#[derive(PartialEq, Debug)]
+enum Instruction {
+    Multiply(usize, usize),
+    Do,
+    Dont,
+}
+
+#[derive(PartialEq, Debug)]
+struct Instructions(Vec<Instruction>);
+
+impl Instructions {
+    fn process(&self) -> usize {
+        let mut do_multiply = true;
+        let mut total = 0;
+        for instruction in &self.0 {
+            match instruction {
+                Instruction::Multiply(a, b) => {
+                    if do_multiply {
+                        total += a * b;
+                    }
+                }
+                Instruction::Do => do_multiply = true,
+                Instruction::Dont => do_multiply = false,
+            }
+        }
+        total
+    }
+}
+
+fn parse_mul(input: &str) -> IResult<&str, Instruction, ErrorTree<&str>> {
+    delimited(
+        tag("("),
+        separated_pair(
+            map_res(digit1, str::parse),
+            tag(","),
+            map_res(digit1, str::parse),
+        ),
+        tag(")"),
+    )
+    .map(|(a, b)| Instruction::Multiply(a, b))
+    .preceded_by(tag("mul"))
+    .parse(input)
+}
+
+fn parse_do(input: &str) -> IResult<&str, Instruction, ErrorTree<&str>> {
+    tag("do()").map(|_| Instruction::Do).parse(input)
+}
+
+fn parse_dont(input: &str) -> IResult<&str, Instruction, ErrorTree<&str>> {
+    tag("don't()").map(|_| Instruction::Dont).parse(input)
+}
+
+fn parse_instruction(input: &str) -> IResult<&str, Instruction, ErrorTree<&str>> {
+    parse_mul.or(parse_do).or(parse_dont).parse(input)
+}
+
+fn parse_line_basic(input: &str) -> IResult<&str, Instructions, ErrorTree<&str>> {
+    many1(many_till(anychar, parse_mul))
+        .terminated(not_line_ending)
+        .parse(input)
+        .map(|(remainder, result)| {
+            let instructions = Instructions(result.into_iter().map(|(_s, m)| m).collect());
+            (remainder, instructions)
+        })
+}
+
+fn run_line_basic(input: &str) -> usize {
+    let (_, list) = parse_line_basic(input).expect("failed to parse line");
+    list.process()
+}
+
+fn parse_line_do_dont(input: &str) -> IResult<&str, Instructions, ErrorTree<&str>> {
+    many1(many_till(anychar, parse_instruction))
+        .terminated(not_line_ending)
+        .parse(input)
+        .map(|(remainder, result)| {
+            let instructions = Instructions(result.into_iter().map(|(_s, m)| m).collect());
+            (remainder, instructions)
+        })
+}
+
+fn run_line_do_dont(input: &str) -> usize {
+    let (_, list) = parse_line_do_dont(input).expect("failed to parse line");
+    list.process()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[ignore]
     #[test]
-    fn test_part1() {
-        let input = "";
-        assert_eq!(AdventPuzzle::part1(input), "");
+    fn test_parse_mul() {
+        let (_, pair) = parse_mul("mul(1,2)").expect("failed to parse mul");
+        assert_eq!(pair, Instruction::Multiply(1, 2));
+        assert!(parse_mul("mul(1, 2)").is_err())
     }
 
-    #[ignore]
+    #[test]
+    fn test_parse_line_basic() {
+        let (_, pairs) = parse_line_basic(
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+        )
+        .expect("failed to parse mul");
+        assert_eq!(
+            pairs,
+            Instructions(vec![
+                Instruction::Multiply(2, 4),
+                Instruction::Multiply(5, 5),
+                Instruction::Multiply(11, 8),
+                Instruction::Multiply(8, 5)
+            ])
+        );
+    }
+
+    #[test]
+    fn test_parse_line_do_dont() {
+        let (_, pairs) = parse_line_do_dont(
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+        )
+        .expect("failed to parse mul");
+        assert_eq!(
+            pairs,
+            Instructions(vec![
+                Instruction::Multiply(2, 4),
+                Instruction::Dont,
+                Instruction::Multiply(5, 5),
+                Instruction::Multiply(11, 8),
+                Instruction::Do,
+                Instruction::Multiply(8, 5)
+            ])
+        );
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(AdventPuzzle::part1(input), "161");
+    }
+
     #[test]
     fn test_part2() {
-        let input = "";
-        assert_eq!(AdventPuzzle::part2(input), "");
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(AdventPuzzle::part2(input), "48");
     }
 }
